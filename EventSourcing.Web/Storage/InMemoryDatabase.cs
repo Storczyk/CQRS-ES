71b2d784ad26ad395165a04e42c83fae4098467c
PGS-SOFT\psroczyk <psroczyk@pgs-soft.com>
2018-03-02 15:29:45 +0100

    no message

--- EventSourcing.Web/Storage/InMemoryDatabase.cs
+++ EventSourcing.Web/Storage/InMemoryDatabase.cs
@@ -2,12 +2,13 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using EventSourcing.Web.Clients.Domain.Clients;
 using EventSourcing.Web.ClientsContracts.Queries;
 
 namespace EventSourcing.Web.Storage
 {
     public static class InMemoryDatabase
     {
-        public static readonly Dictionary<Guid, ClientItem> Details = new Dictionary<Guid, ClientItem>();
+        public static readonly Dictionary<Guid, Client> Details = new Dictionary<Guid, Client>();
     }
 }
